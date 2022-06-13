930f9ab6e526c80f3804f58917700f4110266d9e
swangi-kumari <85639103+swangi-kumari@users.noreply.github.com>
2022-06-13 23:13:26 +0530

    Add files via upload

---
+++ Sorting/src/MissingNumber.java
@@ -0,0 +1,42 @@
+//https://leetcode.com/problems/missing-number/
+
+
+
+//https://leetcode.com/problems/missing-number/
+//Amazon question
+
+import java.util.Arrays;
+
+class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr = {4,0,2,1};
+
+        System.out.println(missingNumber(arr));
+
+    }
+
+    public static int missingNumber(int[] arr){
+        int i=0;
+        while (i<arr.length){
+            int correct = arr[i] ;
+            if(arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr,i,correct);
+            } else {
+                i++;
+            }
+        }
+        //search for 1st missing number
+        for(int index =0; index<arr.length; index++){
+            if(arr[index] != index){
+                return index;
+            }
+        }
+        //case2
+        return arr.length;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
